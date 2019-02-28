c88e3b38c4e0b8460a20de4c2b2be80f6f63d7ac
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2019-02-27 21:37:15 -0800

    Add files via upload

---
+++ application/view/user/failedupgrade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <h1>Something Broke</h1>
+
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+    <div class="box">
+        <h2>What to Do.</h2>
+        <p>
+			Please report this to the site owner what you just did at Hopalonghacksaw@gmail.com.
+			We will try to get this fixed as soon as possable. There really should be no reason for things 
+			to break but who knows programming is hard. 
+        </p>
+
+    </div>
+</div>
