3e65c508ec05586d5177e758d66d462e078ac4d6
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2019-02-27 21:36:58 -0800

    Update changeUserRole.php

--- application/view/user/changeUserRole.php
+++ application/view/user/changeUserRole.php
@@ -1,31 +1,43 @@
 <div class="container">
-    <h1>UserController/changeUserRole</h1>
+    <h1>Upgrade to Business</h1>
 
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
-
+	<?php $potentalvalue = 0; $counter = 0; foreach ($this->users as $user){ $counter++; $potentalvalue = (int)$user->pay + $potentalvalue;} $j = $counter; ?>
+        <h3>Balance: $ <?= UserModel::getBalance(Session::get('user_id')); ?></h3>
+		<h3>Number of Jobs Todo: <?php echo $j; ?></h3>
+		<h3>Number of Jobs Done: <?php echo $this->totaljobs; ?></h3>
+		<h3>Potental Value In Jobs: $<?php echo $potentalvalue; ?></h3>
     <div class="box">
-        <h2>Change account type</h2>
+        <h2>Upgrade to Business</h2>
         <p>
-            This page is a basic implementation of the upgrade-process.
-            User can click on that button to upgrade their accounts from
-            "basic account" to "premium account". This script simple offers
-            a click-able button that will upgrade/downgrade the account instantly.
-            In a real world application you would implement something like a
-            pay-process.
+			Upgrade to Business . You Get many perks such as an unlimited access to jobs.
+			You also get reduced shipping rates for products that you commission. You also
+			get a higher Pay Per product. You also will be consulted about new features
+			and what people want from the site. The Main Perk of Business is that you get
+			notified of all jobs that where no claimed after 48 hours. 
+			<br>
+			<br>
+			<b> Price: $50.00</b>
+			(Will be subtracted from user balance)
         </p>
-	    <p>
-		    Please note: This whole process has been renamed from AccountType (v3.0) to UserRole (v3.1).
-	    </p>
-
-        <h2>Currently your account type is: <?php echo Session::get('user_account_type'); ?></h2>
-        <!-- basic implementation for two account types: type 1 and type 2 -->
+		<script>
+		if(document.getElementById('yeet').value > 50){
+			var x = 'yeet';
+		}else{
+			document.getElementById('button').btn.disabled = true;
+		}
+		</script>
+		<input type ="hidden" value="<?= UserModel::getBalance(Session::get('user_id')); ?>" id="yeet">
 	    <form action="<?php echo Config::get('URL'); ?>user/changeUserRole_action" method="post">
             <?php if (Session::get('user_account_type') == 1) { ?>
-                <input type="submit" name="user_account_upgrade" value="Upgrade my account (to Premium User)" />
+                <input type="submit" name="user_account_upgrade" id = "button" value="Upgrade my account (to Premium User) Will Charge $20 Dollars" />
 	        <?php } else if (Session::get('user_account_type') == 2) { ?>
-	            <input type="submit" name="user_account_downgrade" value="Downgrade my account (to Basic User)" />
+			<h3> You are already A Business User And There is no need to updgrade</h3>
 	        <?php } ?>
 	    </form>
+        <h2>Currently your account type is: <?php echo Session::get('user_account_type'); ?></h2>
+        <!-- basic implementation for two account types: type 1 and type 2 -->
+
     </div>
 </div>
