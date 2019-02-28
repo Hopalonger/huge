6a050e51e279936e9c7ad90812f5a1018eefa5a3
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2019-02-27 21:31:42 -0800

    Update index.php
    Added In Some More Rows

--- application/view/profile/index.php
+++ application/view/profile/index.php
@@ -1,30 +1,36 @@
 <div class="container">
-    <h1>ProfileController/index</h1>
+    <h1>All User's</h1>
     <div class="box">
 
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
 
-        <h3>What happens here ?</h3>
+        <h3>View all users</h3>
         <div>
-            This controller/action/view shows a list of all users in the system. You could use the underlying code to
-            build things that use profile information of one or multiple/all users.
+            See All Users and See What tasks they have completed. See Some of the highest rated workers on the site simple as that.
+			See what other people are doing and current projects
         </div>
         <div>
             <table class="overview-table">
                 <thead>
                 <tr>
-                    <td>Id</td>
+                    
                     <td>Avatar</td>
                     <td>Username</td>
                     <td>User's email</td>
-                    <td>Activated ?</td>
+					<td>Number of Jobs Completed</td>
+					<td>Qualifed for Welding</td>
+					<td>Qualifed for Soldering</td>
+					<td>3D Prining Certifed</td>
+					<td>CNC Certifed</td>
+                    <td>Activated (yes/no)</td>
                     <td>Link to user's profile</td>
                 </tr>
                 </thead>
-                <?php foreach ($this->users as $user) { ?>
+				
+                <?php $extension = $this->extension; foreach ($this->users as $user) { ?>
                     <tr class="<?= ($user->user_active == 0 ? 'inactive' : 'active'); ?>">
-                        <td><?= $user->user_id; ?></td>
+                        <?php $id = $user->user_id; ?>
                         <td class="avatar">
                             <?php if (isset($user->user_avatar_link)) { ?>
                                 <img src="<?= $user->user_avatar_link; ?>" />
@@ -32,6 +38,11 @@
                         </td>
                         <td><?= $user->user_name; ?></td>
                         <td><?= $user->user_email; ?></td>
+						<td><?= $extension[$id]->totaljobs; ?></td>
+						<td><?php if($extension[$id]->weld == 1){echo "Yes";}else{echo "No";}?></td>
+						<td><?php if($extension[$id]->solder == 1){echo "Yes";}else{echo "No";}?></td>
+						<td><?php $prints = "3dprints"; if($extension[$id]->$prints == 1){echo "Yes";}else{echo "No";}?></td>
+						<td><?php if($extension[$id]->cnc == 1){echo "Yes";}else{echo "No";}?></td>
                         <td><?= ($user->user_active == 0 ? 'No' : 'Yes'); ?></td>
                         <td>
                             <a href="<?= Config::get('URL') . 'profile/showProfile/' . $user->user_id; ?>">Profile</a>
