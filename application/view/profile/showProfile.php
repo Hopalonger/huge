c122b3568b7b431ffe77eeefa3016011aab774ea
Hopalonger <34294271+Hopalonger@users.noreply.github.com>
2019-02-27 21:32:13 -0800

    Update showProfile.php
    Changed Up A Few Things And Added In Reviews

--- application/view/profile/showProfile.php
+++ application/view/profile/showProfile.php
@@ -1,11 +1,11 @@
 <div class="container">
-    <h1>ProfileController/showProfile/:id</h1>
+    <h1><?= $this->user->user_name; ?>'s Profile</h1>
     <div class="box">
 
         <!-- echo out the system feedback (error and success messages) -->
         <?php $this->renderFeedbackMessages(); ?>
 
-        <h3>What happens here ?</h3>
+        <h3> All About <?= $this->user->user_name; ?></h3>
         <div>This controller/action/view shows all public information about a certain user.</div>
 
         <?php if ($this->user) { ?>
@@ -13,7 +13,7 @@
                 <table class="overview-table">
                     <thead>
                     <tr>
-                        <td>Id</td>
+                        
                         <td>Avatar</td>
                         <td>Username</td>
                         <td>User's email</td>
@@ -22,7 +22,7 @@
                     </thead>
                     <tbody>
                         <tr class="<?= ($this->user->user_active == 0 ? 'inactive' : 'active'); ?>">
-                            <td><?= $this->user->user_id; ?></td>
+                            
                             <td class="avatar">
                                 <?php if (isset($this->user->user_avatar_link)) { ?>
                                     <img src="<?= $this->user->user_avatar_link; ?>" />
@@ -36,6 +36,24 @@
                 </table>
             </div>
         <?php } ?>
-
+		<style>
+		#box {
+			background-color: #d4d7db;
+			
+		}
+		</style>
+			<h1> Reviews: </h1>
+			<?php foreach ($this->reviews as $user){?>
+			<div id='box'>
+			<center>
+			<h3><?= $user->title; ?></h3>
+			<h4> | Rating: <?= $user->stars; ?>/5  |  Manufacturing Process: <?= $user->process; ?>  |  Timeliness: <?= $user->timely; ?>/10  | Overall Quality: <?= $user->quality; ?>/10 |  Communication: <?= $user->communication; ?>/10  |<h4>
+			<p> <br> </p>
+			<p> <?= $user->body; ?></p>
+			<br>
+			</center>
+			</div>
+			<br>
+			<?php } ?>
     </div>
 </div>
